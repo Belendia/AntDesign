382ccfca287f67aaa769f975c74db3db7afbd943
Belendia Serda <belendia@gmail.com>
2022-11-15 21:35:35 +0300

    Add about page

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import AppAbout from "../components/home/about";
 import AppHero from "../components/home/hero";
 
 function AppHome() {
   return (
     <div className="main">
       <AppHero />
+      <AppAbout />
     </div>
   );
 }
